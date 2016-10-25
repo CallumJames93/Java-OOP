8b98651de434441fa17d7f1f1e344e13c1587622
CallumJames93 <callum.gill1993@hotmail.co.uk>
2016-10-25 18:28:16 +0100

    Inheritance
    Added Inheritance tutorial classes.

---
+++ Audi.java
@@ -0,0 +1,20 @@
+package inheritance;
+
+public class Audi extends Car {
+	
+	public int speed = 150;
+
+	public static void main(String[] args) {
+		Audi A = new Audi();
+		A.printdetails();
+		System.out.println("Color Of Audi = "+A.color);
+		System.out.println("Fuel Of Audi = "+A.getfuel());
+	}
+	
+    public void printdetails(){
+    	 System.out.println("Wheels Of Audi = "+wheels);
+    	 System.out.println("Speed Of Audi = "+speed);
+    	 Seats();
+    }
+
+}
---
+++ Car.java
@@ -0,0 +1,19 @@
+package inheritance;
+
+public class Car {
+
+	private String type = "Vehicle";
+	public static int wheels = 4;
+	public String color = "blue";
+	String fuel = "Petrol";
+	
+	public String getfuel(){
+		return fuel;
+	}
+	
+	protected void Seats(){
+		int seat = 4;
+		System.out.println("Car seats = " + seat);
+	}
+	
+}
